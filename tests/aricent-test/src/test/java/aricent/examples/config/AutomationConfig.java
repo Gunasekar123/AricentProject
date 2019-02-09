770ed900568b6edb7a7ed7921cfc69465d0d4cf9
Gunasekar123 <47343570+Gunasekar123@users.noreply.github.com>
2019-02-09 21:54:04 +0530

    Create AutomationConfig.java

---
+++ tests/aricent-test/src/test/java/aricent/examples/config/AutomationConfig.java
@@ -0,0 +1,31 @@
+public class AutomationConfig{
+
+private String applicationUrl=null;
+
+private String browserType=null;
+
+
+public void load(){
+ Properties properties = new Properties();
+    File currentDirectory = new File(new File("").getAbsolutePath());
+    WORKING_DIR = currentDirectory.getAbsolutePath();
+    if (!(new File(WORKING_DIR + "\\src\\test\\java\\aricent\\examples\\resources\\Environments\\Sample.properties").exists())) {
+      System.out
+          .println("ERROR:There is no environment property file named Nightly.Please specify the correct environment");
+    }
+    properties.load(new FileInputStream(new File(WORKING_DIR
+        + "\\src\\test\\java\\aricent\\examples\\resources\\Environments\\Sample.properties")));
+   
+   setAppilicationUrl();
+   setBrowserType();
+
+}
+
+public void setAppilicationUrl(String applicationUrl){
+this.applicationUrl=applicationUrl;
+}
+
+public void setBrowserType(String browserType){
+this.browserType=browserType;
+}
+}
