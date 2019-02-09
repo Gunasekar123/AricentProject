2861b4e38c8e0d77c43f70d11bb0ad59ef2a5095
Gunasekar123 <47343570+Gunasekar123@users.noreply.github.com>
2019-02-09 20:41:02 +0530

    Update SeleniumMethods.java

--- tests/aricent-test/src/test/java/aricent/examples/utils/SeleniumMethods.java
+++ tests/aricent-test/src/test/java/aricent/examples/utils/SeleniumMethods.java
@@ -1,1 +1,19 @@
+import org.openqa.selenium.WebDriver;
 
+public SeleniumMethods {
+
+  WebDriver driver=null;
+  
+  SeleniumMethods(WebDriver driver){
+    this.driver=driver;
+  }
+
+  public void click(WebElement element){
+   driver.element.click(); 
+  }
+  
+ public void type(WebElement element,String text){
+  driver.element.sendKeys(text); 
+ }
+
+}
