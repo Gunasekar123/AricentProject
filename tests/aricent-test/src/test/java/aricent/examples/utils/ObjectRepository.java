afb026180363458db5a97035e039187e8aa22165
Gunasekar123 <47343570+Gunasekar123@users.noreply.github.com>
2019-02-09 20:07:26 +0530

    Update ObjectRepository.java

--- tests/aricent-test/src/test/java/aricent/examples/utils/ObjectRepository.java
+++ tests/aricent-test/src/test/java/aricent/examples/utils/ObjectRepository.java
@@ -1,1 +1,30 @@
 
+public class ObjectRepository {
+  
+  WebDriver driver=null;
+  ObjectRepository(WebDriver driver){
+    this.driver=driver;
+  }
+  
+
+/**
+*Login page Web Elements 
+**/
+  WebElement  userName= driver.findElement(By.id(" "));
+  WebElement  password= driver.findElement(By.id(" "));
+  WebElement  clickOnLogin= driver.findElement(By.id(" "));
+  
+  public WebElement getUserName(){
+    return userName;
+  }
+  public WebElement getPassword(){
+    return userName;
+  }
+  public WebElement getLogin(){
+    return userName;
+  }
+  
+  
+  
+}
+  
