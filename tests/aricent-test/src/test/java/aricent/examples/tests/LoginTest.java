c37ce260f1db2c76f8c6e99264c7cbf2c3c2cc3d
Gunasekar123 <47343570+Gunasekar123@users.noreply.github.com>
2019-02-10 10:33:31 +0530

    Update LoginTest.java

--- tests/aricent-test/src/test/java/aricent/examples/tests/LoginTest.java
+++ tests/aricent-test/src/test/java/aricent/examples/tests/LoginTest.java
@@ -1,4 +1,9 @@
-
+package aricent.examples.tests;
+import aricent.examples.tests.BaseClass;
+import aricent.examples.tests.PageSpecific.LoginPage;
+/**
+*Test to verify Login page 
+**/
 public class LoginTest extends BaseClass{
   WebDriver driver=null;
   /**
@@ -19,6 +24,7 @@ public class LoginTest extends BaseClass{
   public void baseSetup(){
    logger.info("Setting up the environment");
     driver=super.getDriver();
+    logger.info("Loadind login page");
     super.loadLoginPage(driver);
   }
   /**
@@ -27,10 +33,12 @@ public class LoginTest extends BaseClass{
   @Test
   public void testLogin(){
   LoginPage lp=new LoginPage(driver);
+   logger.info("Doing signIn");
    lp.doSignIn("Guna","myPass");
-   lp.validateSucessfullSignIn();
-    
-    
+   logger.info("Completed signIn");
+   logger.info("Validating the page with expected mesage after login");
+   lp.validateSucessfullSignIn("//loginPageUrl","Logged in Successfully");
+   logger.info("Page validation is completed");
   }
   /**
   *Closing driver
@@ -38,11 +46,8 @@ public class LoginTest extends BaseClass{
   **/
   @AfterClass
   public tearDown(){
-    
+    logger.info("End of test , Closing the driver ");
+    driver.close();
   }
-}
-
-  
-  
-  
+}  
 }
