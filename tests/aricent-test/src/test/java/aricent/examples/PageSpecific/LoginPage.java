771657139e984c762e8f92fc26e636e0caa82636
Gunasekar123 <47343570+Gunasekar123@users.noreply.github.com>
2019-02-10 10:43:38 +0530

    Update LoginPage.java

--- tests/aricent-test/src/test/java/aricent/examples/PageSpecific/LoginPage.java
+++ tests/aricent-test/src/test/java/aricent/examples/PageSpecific/LoginPage.java
@@ -1,17 +1,31 @@
+package aricent.examples.PageSpecific;
 import org.openqa.selenium.WebDriver;
+/**
+*This page containd login page methods 
+**/
 public class LoginPage extends SeleniumMethods{
  WebDriver driver=null;
  LoginPage(WebDriver driver){
  super(driver);
  this.driver=driver;
  }
+ /**
+ *Creating Object repository object 
+ **/
  ObjectRepository obj=new ObjectRepository(driver);
+ /**
+ *It will do signIn 
+ *This method will take two parameters as a argument 
+ **/
  public LoginPage doSignIn(String userName,String password){
     type(obj.getUserName(),userName);
     type(obj.getPassword(),password); 
     click(obj.getSignIn());
   return this;
 }
+ /**
+ *This is to validate page message after sucessfull sigin
+ **/
  public LoginPage validateSucessfullSignIn(String expectedPageUrl,String messageToValidate){
   if(driver.getCurrentUrl().equals(expectedPageUrl))
      {
