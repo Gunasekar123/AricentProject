e60caa8f7a0ec68ece77cc211d04ad2c5a94f53b
Gunasekar123 <47343570+Gunasekar123@users.noreply.github.com>
2019-02-10 09:46:01 +0530

    Update LoginTest.java

--- tests/aricent-test/src/test/java/aricent/examples/tests/LoginTest.java
+++ tests/aricent-test/src/test/java/aricent/examples/tests/LoginTest.java
@@ -1,1 +1,48 @@
 
+public class LoginTest extends BaseClass{
+  WebDriver driver=null;
+  /**
+  *Implementing abstract method of base class
+  **/
+  @Override
+  public String getLogName(){
+    return LoginTest.class.getName();
+  }
+  /***
+  *Initilizing logger
+  **/
+  TestCaseLogger logger=new TestCaseLogger(getLogName());
+  /**
+  *Setting up the environment to test login
+  **/
+  @BeforeClass
+  public void baseSetup(){
+   logger.info("Setting up the environment");
+    driver=super.getDriver();
+    super.loadLoginPage(driver);
+  }
+  /**
+  *Testing login and validating successful login message 
+  **/
+  @Test
+  public void testLogin(){
+  LoginPage lp=new LoginPage(driver);
+   lp.doSignIn("Guna","myPass");
+   lp.validateSucessfullSignIn();
+    
+    
+  }
+  /**
+  *Closing driver
+  *Closing browser
+  **/
+  @AfterClass
+  public tearDown(){
+    
+  }
+}
+
+  
+  
+  
+}
